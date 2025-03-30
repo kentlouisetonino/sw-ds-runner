8b0e5b9bf03f672d5e2520105b23dbf7d18873b7
Kent Tonino <tonino.kentlouise@gmail.com>
2025-03-30 11:50:29 +0800

    Handle invalid input.

--- src/queue/queue.cpp
+++ src/queue/queue.cpp
@@ -1,42 +1,69 @@
 #include "../helper/helper.hpp"
 
 void queue(void) {
-  // Cleanup the terminal.
-  clear_terminal();
-  new_lines(2);
+  bool is_option_invalid = false;
 
-  // Show the option description.
-  blue_text("-----------------------------------------------------", 7);
-  new_lines(2);
-  green_text("Queue", 29);
-  new_lines(2);
-  white_text("A Data Structure that follows FIFO order.", 13);
-  new_lines(2);
-  blue_text("-----------------------------------------------------", 7);
-  new_lines(3);
+  while (true) {
+    // Cleanup the terminal.
+    clear_terminal();
+    new_lines(2);
 
-  // Shwo the instructions.
-  yellow_text("Please choose an operation for Queue:", 8);
-  new_lines(3);
+    // Show the option description.
+    blue_text("-----------------------------------------------------", 7);
+    new_lines(2);
+    green_text("Queue", 29);
+    new_lines(2);
+    white_text("A Data Structure that follows FIFO order.", 13);
+    new_lines(2);
+    blue_text("-----------------------------------------------------", 7);
+    new_lines(3);
 
-  // Show the queue options.
-  option_formatter("1", "Enqueue", 8);
-  new_lines(1);
-  option_formatter("2", "Dequeue", 8);
-  new_lines(1);
-  option_formatter("3", "Display", 8);
-  new_lines(1);
-  option_formatter("3", "Check Front", 8);
-  new_lines(1);
-  option_formatter("4", "Check Rear", 8);
-  new_lines(1);
-  option_formatter("5", "Exit", 8);
-  new_lines(3);
+    // Show the instructions.
+    yellow_text("Please choose an operation for Queue:", 8);
+    new_lines(3);
 
-  // Handle the input.
-  int input;
-  white_text("Enter data structure option: ", 8);
-  std::cin >> input;
+    // Show the error message.
+    if (is_option_invalid) {
+      option_error_formatter("Invalid input. Please try again.", 8);
+      new_lines(3);
+    }
 
-  // TODO: handle the operations here.
+    // Show the queue options.
+    option_formatter("1", "Enqueue", 8);
+    new_lines(1);
+    option_formatter("2", "Dequeue", 8);
+    new_lines(1);
+    option_formatter("3", "Display", 8);
+    new_lines(1);
+    option_formatter("4", "Check Front", 8);
+    new_lines(1);
+    option_formatter("5", "Check Rear", 8);
+    new_lines(1);
+    option_formatter("6", "Exit", 8);
+    new_lines(3);
+
+    // Handle the input.
+    int input;
+    white_text("Enter data structure option: ", 8);
+    std::cin >> input;
+    clear_input_buffer();
+
+    // Handle which operations to show.
+    if (input == 1) {
+      break;
+    } else if (input == 2) {
+      break;
+    } else if (input == 3) {
+      break;
+    } else if (input == 4) {
+      break;
+    } else if (input == 5) {
+      break;
+    } else if (input == 6) {
+      break;
+    } else {
+      is_option_invalid = true;
+      continue;
+    }
+  }
 }
