d26608ef609b8a61f81491ff2f3f6060265f8d09
Kent Tonino <tonino.kentlouise@gmail.com>
2025-04-01 20:18:08 +0800

    Correct the spelling.

--- src/home/show_home_instruction.cpp
+++ src/home/show_home_instruction.cpp
@@ -1,5 +1,5 @@
 #include "../helper/helper.hpp"
 
 void show_home_instruction(int width) {
-  yellow_text("Please choose an number for a Data Structure:", width);
+  yellow_text("Please choose a number for a Data Structure:", width);
 }
