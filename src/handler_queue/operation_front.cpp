4ee4ea1e6a92f22bb56ed687ba1077bef36bf79a
Kent Tonino <tonino.kentlouise@gmail.com>
2025-04-14 19:23:09 +0800

    Implement show front queue element.

---
+++ src/handler_queue/operation_front.cpp
@@ -0,0 +1,39 @@
+#include "../helper/helper.hpp"
+#include <queue>
+#include <string>
+using namespace std;
+
+void operation_front(queue<string> queue_storage) {
+  while (true) {
+    // Cleanup the terminal.
+    clear_terminal();
+    new_lines(3);
+
+    // Show the option description.
+    blue_text("-----------------------------------------------------", 7);
+    new_lines(2);
+    green_text("Front Operation", 26);
+    new_lines(2);
+    white_text("Show the current front element in Queue.", 14);
+    new_lines(2);
+    blue_text("-----------------------------------------------------", 7);
+    new_lines(3);
+
+    // Show the current front queue.
+    white_text("Front element:", 8);
+    green_text(queue_storage.front(), 1);
+    new_lines(3);
+
+    // Handle adding another values.
+    char try_again;
+    white_text("Show again? [y/n]: ", 8);
+    std::cin >> try_again;
+    clear_input_buffer();
+
+    if (try_again == 'y') {
+      continue;
+    } else {
+      break;
+    }
+  }
+}
