381a1a914c2c444f15d91d5777a6137b201879ff
Kent Tonino <tonino.kentlouise@gmail.com>
2025-05-23 22:09:33 +0800

    Fix import in handler_binary_tree.cpp file.

--- src/handler_binary_tree/handler_binary_tree.hpp
+++ src/handler_binary_tree/handler_binary_tree.hpp
@@ -1,6 +1,6 @@
 #ifndef HANDLER_BINARY_TREE_HPP
 #define HANDLER_BINARY_TREE_HPP
-#include "./handler_binary_tree.cpp"
+#include "handler_binary_tree.cpp"
 
 extern void handler_binary_tree(void);
 
