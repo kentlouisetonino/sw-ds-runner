99e35989986f54e2a1d146e79cf002835133ef90
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2025-05-24 18:07:45 +0800

    Remove unnecessary comments.

--- src/handler_home/handler_home.cpp
+++ src/handler_home/handler_home.cpp
@@ -5,29 +5,23 @@
 #include "show_home_options.cpp"
 
 int handler_home(bool has_error) {
-  // Cleanup the terminal.
   clear_terminal();
   new_lines(3);
 
-  // Computer program description.
   show_home_description(7);
   new_lines(3);
 
-  // Show the instruction in choosing the option.
   show_home_instruction(8);
   new_lines(3);
 
-  // Show error message.
   if (has_error) {
     option_error_formatter("Invalid input. Please try again.", 8);
     new_lines(3);
   }
 
-  // Show the available data structure options.
   show_home_options(8);
   new_lines(2);
 
-  // Show
   int option;
   option = get_home_input(8);
 
